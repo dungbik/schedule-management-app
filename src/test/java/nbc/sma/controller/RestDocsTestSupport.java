becbcbe18d4cf4d475a403b37eb4d28b5c445e39
dungbik <q1w2e30630@gmail.com>
2025-03-20 18:19:28 +0900

    refactor: 불필요한 코드 제거

--- src/test/java/nbc/sma/controller/RestDocsTestSupport.java
+++ src/test/java/nbc/sma/controller/RestDocsTestSupport.java
@@ -1,42 +1,18 @@
 package nbc.sma.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nbc.sma.config.RestDocsConfig;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.test.web.servlet.MockMvc;
 
 @Disabled
-@Import(RestDocsConfig.class)
-@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 public abstract class RestDocsTestSupport {
 
     protected ObjectMapper mapper = new ObjectMapper();
 
-    protected MockMvc mockMvc;
-
     @Autowired
-    protected RestDocumentationResultHandler restDocs;
-
-    @BeforeEach
-    void setUp(final WebApplicationContext context, final RestDocumentationContextProvider provider) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
-                .alwaysDo(print())
-                .alwaysDo(restDocs)
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .build();
-    }
+    protected MockMvc mockMvc;
 }
--- src/test/java/nbc/sma/controller/UserControllerTest.java
+++ src/test/java/nbc/sma/controller/UserControllerTest.java
@@ -22,7 +22,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureRestDocs
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest extends RestDocsTestSupport {
 
