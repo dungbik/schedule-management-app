c79731744d07f4660b81846a19c51308dea784c0
dungbik <q1w2e30630@gmail.com>
2025-03-27 10:48:47 +0900

    refactor: 일정 수정 요청 검증 조건 수정

--- src/main/java/nbc/sma/dto/request/EditScheduleRequest.java
+++ src/main/java/nbc/sma/dto/request/EditScheduleRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record EditScheduleRequest(
-        @NotNull @NotBlank @Size(max = 50) String username,
-        @NotNull @NotBlank @Size(max = 50) String task,
-        @NotNull @NotBlank @Size(max = 50) String password
+        @NotNull @NotBlank @Size(max = 50)  String username,
+        @NotNull @NotBlank @Size(max = 200) String task,
+        @NotNull @NotBlank @Size(max = 50)  String password
 ) {
 }
